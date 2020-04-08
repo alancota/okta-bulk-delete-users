d91ec6f9c8a64e97a8b8c379c612bbfb091fe0c5
alancota <alancota@gmail.com>
2020-04-08 18:24:54 -0500

    Fixed some things

--- deleteStaged.py
+++ deleteStaged.py
@@ -15,7 +15,7 @@ def stagedUsers():
 
 # Function to deactivate an User
 def deactivateUser(userID):
-    url = constant.OKTA_ORG + constant.OKTA_API_ENDPOINT + '/' + userID +'/lifecycle/deactivate'
+    url = constant.OKTA_API_ENDPOINT + '/' + userID +'/lifecycle/deactivate'
     resp = requests.request("POST", url, headers=constant.HEADERS, data = constant.PAYLOAD)
     if resp.status_code == 200:
         # Call function to delete user
@@ -24,15 +24,30 @@ def deactivateUser(userID):
     return
 
 def deleteUser(userID):
-    url = constant.OKTA_ORG + constant.OKTA_API_ENDPOINT + '/' + userID
+    url = constant.OKTA_API_ENDPOINT + '/' + userID
     requests.request("DELETE", url, headers=constant.HEADERS, data = constant.PAYLOAD)
+    #print("Deleted user " + userID)
     return
 
-totalUsers = len(stagedUsers())
-while totalUsers != -1:
-    for user in stagedUsers():
-        print user['id']
-        deactivateUser(user['id'])
+
+# Main
+def main():
+    processed = 0
+    loopNumber = 0
     totalUsers = len(stagedUsers())
-else:
-    print("End of loop " + str(totalUsers))
+    while totalUsers != -1:
+        loopNumber += 1
+        print("Starting the loop: ["+str(loopNumber)+"]\n")
+        processed = processed+totalUsers
+        for user in stagedUsers():
+            print("Processing "+user['id'])
+            deactivateUser(user['id'])
+        
+        totalUsers = len(stagedUsers())
+        print("\nFound more " + str(totalUsers)+ ", restarting the loop...")
+        print("----------\n")
+    else:
+     print("End of loop: " + str(processed)+" users deleted")
+
+if __name__ == '__main__':
+    main()
